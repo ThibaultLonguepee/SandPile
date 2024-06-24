26acd193d5632943deadbb35284eb64ad3020566
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-06-24 16:26:10 +0200

    feat(tester): added different build to tests

--- tests/tester.py
+++ tests/tester.py
@@ -25,24 +25,53 @@ class Test:
         self.params = ' '.join(params)
 
     def run(self) -> bool:
-        print(f"[{BLUE}==={RESET}] Running test: {BOLD}{self.input}{RESET}")
+        print(f"\t[{BLUE}==={RESET}] Running test: {BOLD}{self.input}{RESET}")
 
         status = system(f"./sandpile {self.input} {self.params} > /tmp/sandpile_out")
         if status != 0:
-            print(f"[{RED}FAIL{RESET}] {BOLD}Program Failed. Stopping.{RESET}")
+            print(f"\t[{RED}FAIL{RESET}] {BOLD}Program Failed. Stopping.{RESET}")
             return False
 
         status = system(f"diff /tmp/sandpile_out {self.expected}")
         if status != 0:
-            print(f"[{RED}==={RESET}] {BOLD}Output differs. Stopping.{RESET}")
+            print(f"\t[{RED}==={RESET}] {BOLD}Output differs. Stopping.{RESET}")
             return False
-        print(f"[{GREEN}==={RESET}] {BOLD}Test success!{RESET}")
+        print(f"\t[{GREEN}==={RESET}] {BOLD}Test success!{RESET}")
         return True
 
+class Build:
+
+    target = ""
+
+    def __init__(self, target) -> None:
+        self.target = target
+
+    def build(self) -> bool:
+        print(f"[{BLUE}==={RESET}] Building for: {BOLD}{self.target}{RESET}")
+        status = system(f"make {self.target} > /dev/null")
+        if status != 0:
+            print(f"[{RED}==={RESET}] {BOLD}Build failed.{RESET}")
+        return status == 0
+
+    def clean(self) -> None:
+        system("make fclean > /dev/null")
+
 if __name__ == '__main__':
-    tests = [
-        Test('simple_numerals', ['0123', '2'])
+    builds = [
+        Build("DEFAULT"),
+        Build("COPY_COLLAPSE"),
+        Build("SELF_COLLAPSE"),
     ]
-    for test in tests:
-        if not test.run():
+    tests = [
+        Test('simple_numerals', ['0123', '2']),
+    ]
+    for build in builds:
+        if not build.build():
             exit(1)
+        for test in tests:
+            if not test.run():
+                build.clean()
+                exit(1)
+        build.clean()
+        if build != builds[-1]:
+            print('')
